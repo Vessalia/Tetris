b3606b3aca55bfe4dd1ed0f70669514e5225f18e
Vessalia <nathanielples@outlook.com>
2022-03-27 01:59:04 -0500

    rotations flipped

--- Src/Controller.cs
+++ Src/Controller.cs
@@ -34,8 +34,8 @@ namespace Tetris.Src
         {
             configManager = new ConfigManager(data);
 
-            if (input.IsKeyJustPressed(configManager.GetKeyBinding("rotate cw"))) { activeBlock.ClampedRotateClockwise(grid); }
-            if (input.IsKeyJustPressed(configManager.GetKeyBinding("rotate ccw"))) { activeBlock.ClampedRotateCounterClockwise(grid); }
+            if (input.IsKeyJustPressed(configManager.GetKeyBinding("rotate cw"))) { activeBlock.ClampedRotateCounterClockwise(grid); }
+            if (input.IsKeyJustPressed(configManager.GetKeyBinding("rotate ccw"))) { activeBlock.ClampedRotateClockwise(grid); }
             if (input.IsKeyJustPressed(configManager.GetKeyBinding("left"))) { activeBlock.HorizontalTranslation(-1); }
             if (input.IsKeyJustPressed(configManager.GetKeyBinding("right"))) { activeBlock.HorizontalTranslation(1); }
             if (input.IsKeyDown(configManager.GetKeyBinding("down")) && blockRefresh) { activeBlock.VerticalTranslation(1/2); }
